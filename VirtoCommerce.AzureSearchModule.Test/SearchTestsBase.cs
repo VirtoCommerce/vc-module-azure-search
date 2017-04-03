a073ac19e193c8cac7861b812932bc4242246cff
artem-dudarev <ad@virtoway.com>
2017-04-03 16:39:51 +0200

    Added tests

---
+++ VirtoCommerce.AzureSearchModule.Test/SearchTestsBase.cs
@@ -0,0 +1,29 @@
+﻿using VirtoCommerce.AzureSearchModule.Data;
+using VirtoCommerce.SearchModule.Core.Model;
+using VirtoCommerce.SearchModule.Core.Model.Search;
+
+namespace VirtoCommerce.AzureSearchModule.Test
+{
+    public class SearchTestsBase
+    {
+        protected static ISearchProvider GetSearchProvider()
+        {
+            return CreateSearchProvider("", "test");
+        }
+
+        protected static ISearchProvider GetBadSearchProvider()
+        {
+            return CreateSearchProvider("", "test");
+        }
+
+
+        private static ISearchProvider CreateSearchProvider(string dataSource, string scope)
+        {
+            var connection = new SearchConnection(dataSource, scope);
+            var queryBuilder = new AzureSearchQueryBuilder() as ISearchQueryBuilder;
+            var provider = new AzureSearchProvider(connection, new[] { queryBuilder });
+
+            return provider;
+        }
+    }
+}
