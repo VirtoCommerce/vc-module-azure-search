ae7b9cd387094e1c496cd7ec35a68e967fa89df4
artem-dudarev <ad@virtoway.com>
2017-10-18 13:11:13 +0200

    v1.1.3

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Virto Commerce Azure Search Module")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2017")]
 
-[assembly: AssemblyFileVersion("1.1.2.0")]
-[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
