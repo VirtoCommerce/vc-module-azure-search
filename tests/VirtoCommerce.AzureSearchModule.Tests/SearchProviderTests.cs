068ee627983652e3fc46b35ddb0ed296a5c29fbe
Andrei Kostyrin <kostyrin2@gmail.com>
2019-09-18 10:55:13 +0200

    #vp529 [Trait("Category", "IntegrationTest")]

--- tests/VirtoCommerce.AzureSearchModule.Tests/SearchProviderTests.cs
+++ tests/VirtoCommerce.AzureSearchModule.Tests/SearchProviderTests.cs
@@ -6,6 +6,7 @@ using Xunit;
 namespace VirtoCommerce.AzureSearchModule.Tests
 {
     [TestCaseOrderer(PriorityTestCaseOrderer.TypeName, PriorityTestCaseOrderer.AssembyName)]
+    [Trait("Category", "IntegrationTest")]
     public abstract class SearchProviderTests : SearchProviderTestsBase
     {
         public const string DocumentType = "item";
