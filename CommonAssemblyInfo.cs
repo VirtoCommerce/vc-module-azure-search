af93b7e9052c3faa3b09c593553fc02a844b445f
Andrey Kozlov <yecli.github@gmail.com>
2019-12-06 12:15:19 +0500

    1.2.0

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Virto Commerce Azure Search Module")]
 [assembly: AssemblyCopyright("Copyright © VirtoCommerce 2018")]
 
-[assembly: AssemblyFileVersion("1.1.5.0")]
-[assembly: AssemblyVersion("1.1.5.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
