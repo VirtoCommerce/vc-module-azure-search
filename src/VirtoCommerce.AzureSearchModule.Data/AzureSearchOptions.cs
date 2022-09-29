dcd52fc7a3c6d78cc8bf3a61a0a9ef71e9346b6b
Oleg Zhuk <zhukoo@gmail.com>
2022-09-29 10:35:59 +0200

    PT-8565: add fuzzy search support (#30)
    feat: Add Fuzzy Search support.
    fix: Update documentation.

--- src/VirtoCommerce.AzureSearchModule.Data/AzureSearchOptions.cs
+++ src/VirtoCommerce.AzureSearchModule.Data/AzureSearchOptions.cs
@@ -1,8 +1,16 @@
+using Microsoft.Azure.Search.Models;
+
 namespace VirtoCommerce.AzureSearchModule.Data
 {
     public class AzureSearchOptions
     {
         public string SearchServiceName { get; set; }
         public string Key { get; set; }
+        /// <summary>
+        /// Azure Cognitive Search supports two Lucene-based query languages: Simple and Full.
+        /// </summary>
+        /// <seealso cref="https://learn.microsoft.com/en-us/azure/search/query-simple-syntax"/>
+        /// <seealso cref="https://learn.microsoft.com/en-us/azure/search/search-query-lucene-examples"/>
+        public QueryType QueryParserType { get; set; } = QueryType.Simple;
     }
 }
