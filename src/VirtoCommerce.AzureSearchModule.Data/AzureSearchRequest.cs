43a7b8076d4e836c88c5a081e422d00d557fd8ca
Artem Dudarev <artem@virtoworks.com>
2025-10-01 16:13:04 +0200

    VCST-4045: Fix sonar issues

--- src/VirtoCommerce.AzureSearchModule.Data/AzureSearchRequest.cs
+++ src/VirtoCommerce.AzureSearchModule.Data/AzureSearchRequest.cs
@@ -1,6 +1,4 @@
-using System;
 using Azure.Search.Documents;
-using Microsoft.Azure.Search.Models;
 
 namespace VirtoCommerce.AzureSearchModule.Data
 {
@@ -8,10 +6,6 @@ namespace VirtoCommerce.AzureSearchModule.Data
     {
         public string AggregationId { get; set; }
         public string SearchText { get; set; }
-
-        [Obsolete("Use SearchOptions", DiagnosticId = "VC0008", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions/")]
-        public SearchParameters SearchParameters { get; set; }
-
         public SearchOptions SearchOptions { get; set; }
     }
 }
